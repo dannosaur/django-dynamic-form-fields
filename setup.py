df835f9989b08970c52cdc67fdbfb5998dd6f83e
Dan <me@dannosaur.com>
2017-12-01 19:14:36 +0000

    pip setup

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="django-dynamic-form-fields",
@@ -8,6 +8,18 @@ setup(
     url="https://github.com/dannosaur/django-dynamic-form-fields",
     author="dannosaur",
     author_email="me@dannosaur.com",
+    packages=find_packages(),
+    include_package_data=True,
+    # packages=[
+    #     'dynamic_fields',
+    #     'dynamic_fields/static',
+    #     'dynamic_fields/static/dynamic_fields/',
+    #     'dynamic_fields/static/dynamic_fields/js',
+    #     'dynamic_fields/templates',
+    #     'dynamic_fields/templates/dynamic_fields',
+    #     'dynamic_fields/templates/dynamic_fields/forms',
+    #     'dynamic_fields/templates/dynamic_fields/forms/widgets',
+    # ],
     install_requires=[
         'django >= 1.11',
     ],
