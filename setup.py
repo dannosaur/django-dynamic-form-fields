3660db01f97fa1b60e46e1d870025f923c915314
Dan <me@dannosaur.com>
2017-12-01 18:41:34 +0000

    add pip setup stuff

---
+++ setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name="django-dynamic-form-fields",
+    version="0.0.1",
+    url="https://github.com/dannosaur/django-dynamic-form-fields",
+    author="dannosaur",
+    author_email="me@dannosaur.com",
+    install_requires=[
+        'django >= 1.11',
+    ],
+    python_requires='>=3.5',
+)
